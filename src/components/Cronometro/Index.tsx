5e60b71af4ca76a5def7fb4af495187dc0e0e98e
MatheusGrenat <matheusgrenat@yahoo.com.br>
2024-08-12 20:05:09 -0300

    retornando o valor do tempo dentro do cronometro na tela

--- src/components/Cronometro/Index.tsx
+++ src/components/Cronometro/Index.tsx
@@ -17,13 +17,12 @@ export default function Cronometro({selecionado}: Props){
             setTempo(tempoParaSegundos(selecionado.tempo));
         }   
     },[selecionado])
-    
+
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-            Tempo: {tempo}
             <div className={style.relogioWrapper}>
-                <Relogio />
+                <Relogio tempo={tempo}/>
             </div>
             <Botao
                 texto="Começar!"
